efff0d19619e701952f066c48fc2070e3dbc8073
iblank01 <ianblankenship32@gmail.com>
2024-06-08 15:57:03 -0400

    Initial Commit

---
+++ food-truck-ios-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  food-truck-ios-app
+//
+//  Created by Ian Blankenship on 6/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ food-truck-ios-app/food_truck_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  food_truck_ios_appApp.swift
+//  food-truck-ios-app
+//
+//  Created by Ian Blankenship on 6/8/24.
+//
+
+import SwiftUI
+
+@main
+struct food_truck_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
